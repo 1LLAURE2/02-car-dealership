8f118b2a2bda278cde2d24148310efccbfe72122
1LLAURE2 <josellaurellapo@gmail.com>
2026-03-08 12:22:48 -0500

    ValidationPipe

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,10 @@
+import { IsString } from "class-validator";
+
 export class CreateCarDto{
+
+    @IsString({ message: `The band most be a cool string `})
     readonly brand: string;
+
+    @IsString()
     readonly model: string;
 }
